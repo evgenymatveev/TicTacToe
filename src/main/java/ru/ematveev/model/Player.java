7e17c473f30331190f24df3143a6a9cc2408bdb7
evgenymatveev <evgeni2007@yandex.ru>
2016-12-25 22:25:00 +0300

    Player - check no errors

--- src/main/java/ru/ematveev/model/Player.java
+++ src/main/java/ru/ematveev/model/Player.java
@@ -1,20 +1,42 @@
 package ru.ematveev.model;
+/**
+ * class Player create players.
+ */
+public class Player {
 
-public class Player{
-
+	/**
+	 * the figure which the player is playing.
+	 */
 	private final String name;
+
+	/**
+	 * player name.
+	 */
 	private final Figure figure;
 
-	public Player(final String name, final Figure figure){
+	/**
+	 * constructor.
+	 * @param name name.
+	 * @param figure figure.
+	 */
+	public Player(final String name, final Figure figure) {
 		this.name = name;
 		this.figure = figure;
 	}
 
-	public String getName(){
+	/**
+	 * returns the name of the player.
+	 * @return name.
+	 */
+	public String getName() {
 		return name;
 	}
 
-	public Figure getFigure(){
+	/**
+	 * returns the figure of the player.
+	 * @return figure.
+	 */
+	public Figure getFigure() {
 		return figure;
 	}
 }
