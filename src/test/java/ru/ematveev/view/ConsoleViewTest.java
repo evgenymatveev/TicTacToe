14ff2a15e3ff10601ba9e78d8ef03cb5db3a1367
evgenymatveev <evgeni2007@yandex.ru>
2016-12-29 01:07:55 +0300

    view in the test - checkstyle

---
+++ src/test/java/ru/ematveev/view/ConsoleViewTest.java
@@ -0,0 +1,39 @@
+package ru.ematveev.view;
+/**
+ * Class ConsoleViewTest tests the metods of the class ConsoleView.
+ * @author Matveev Evgeny.
+ * @version 1.0.
+ * @since 25.12.2016.
+ */
+
+public class ConsoleViewTest {
+    /*@Test
+    public void testGetMoveCoordinate() throws Exception {
+        FieldSmall inputfieldSmall = new FieldSmall();
+
+        final Point expectedValue = new Point(0, 0);
+
+        ConsoleView consoleView = new ConsoleView();
+
+        final Point actualValue = consoleView.getMoveCoordinate(inputfieldSmall);
+
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void testShow() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateLine() throws Exception {
+
+    }
+
+    @Test
+    public void testGenerateSeparator() throws Exception {
+
+    }*/
+
+}
